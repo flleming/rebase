9e5bfe90fe2ac3a3e5168ac6ca373fce1e1e490f
Omar Hamza <omar.hamza@csdental.com>
2023-01-25 11:41:03 +0100

    dqsd

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const path= require("path")
+const cors=require('cors')
 const app = express()
-const port = 3000
-
+const port = 4000
+app.use(cors())
 app.set("view engine","ejs")
 app.set("views",'./views')
 app.get('/', (req, res) => {
-  res.render("index",{name:"hello",age:14})
+ return res.render("index",{name:"hello",age:14})
+ 
+})
+app.get('/api/v1', (req, res) => {
+  // res.render("index",{name:"hello",age:14})
+  return res.json([
+    {email:"lab1@email.com"},
+    {email:"lab2@email.com"},
+    {email:"lab3@email.com"},
+  ])
 })
 
 app.listen(port, () => {
